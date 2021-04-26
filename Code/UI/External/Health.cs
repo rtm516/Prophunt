f85c5c12149dbd449a85095abe1623cff1d915a6
Isaiah <44090193+accurateisaiah@users.noreply.github.com>
2021-04-26 08:00:42 -0400

    Added Dynamic Health System. (fix code please)

--- Code/UI/External/Health.cs
+++ Code/UI/External/Health.cs
@@ -1,25 +1,32 @@
-﻿using Sandbox;
+using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 
 namespace Prophunt.UI.External
 {
-	public class Health : Panel
-	{
-		public Label Label;
+    public class Health : Panel
+    {
+        public Label Label;
 
-		public Health()
-		{
-			Add.Label( "🩸", "icon" );
-			Label = Add.Label( "100", "value" );
-		}
+        public Health()
+        {
+            object Health2 = Add.Label("💓", "icon");
+            Label = Add.Label("100", "value");
+            if (player.Health) = 10f;
+                    {
+                Health2 = Add.Label("💔", "icon";)
+            }
 
-		public override void Tick()
-		{
-			var player = Player.Local;
-			if ( player == null ) return;
 
-			Label.Text = $"{player.Health:n0}";
-		}
-	}
+
+        }
+
+        public override void Tick()
+        {
+            var player = Player.Local;
+            if (player == null) return;
+
+            Label.Text = $"{player.Health:n0}";
+        }
+    }
 }
