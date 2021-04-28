daee42d1c9766600bff8d8396c229cdeebd0eeb7
rtm516 <ryantmilner@hotmail.co.uk>
2021-04-29 00:46:54 +0100

    Add ph_prop_physics

---
+++ Code/Entities/ProphuntProp.cs
@@ -0,0 +1,11 @@
+﻿using Prophunt.Players;
+using Prophunt.Utils;
+using Sandbox;
+
+namespace Prophunt.Entities
+{
+	[Library( "ph_prop_physics" )]
+	public partial class ProphuntProp : Prop
+	{
+	}
+}
