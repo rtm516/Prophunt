8f790da30f957a0c9bdb84a40be0db98c9318d94
rtm516 <ryantmilner@hotmail.co.uk>
2021-05-02 21:39:46 +0100

    Fix alignment of text

--- Code/UI/PropLockAlert.cs
+++ Code/UI/PropLockAlert.cs
@@ -20,15 +20,6 @@ namespace Prophunt.UI
 		{
 			if ( Player.Local is not ProphuntPlayer player ) return;
 
-			if ( player.Team != Team.Prop )
-			{
-				Style.Display = DisplayMode.None;
-			}
-			else
-			{
-				Style.Display = DisplayMode.Flex;
-			}
-
 			SetClass( "PropLockOn", player.Locked );
 
 			Style.Dirty();
